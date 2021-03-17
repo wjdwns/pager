041f3f1239ad1c3351f3c125ac5c6d1372975a6d
wjdwns <wjdwns3744@naver.com>
2021-03-17 17:19:01 +0900

    pager 정복 근데 왜 Toast가 안뜰까...?

---
+++ app/src/main/java/com/example/pager/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.pager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        pager.adapter = PagerAdapter(this)
+        pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        pager.offscreenPageLimit = 3
+
+        pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                
+                showToast("${position + 1} 페이지 선택됨")
+            }
+        })
+        button1.setOnClickListener(){
+            pager.currentItem =0
+        }
+        button2.setOnClickListener(){
+            pager.currentItem =1
+        }
+        button3.setOnClickListener(){
+            pager.currentItem =2
+        }
+    }
+        inner class PagerAdapter : FragmentStateAdapter {
+            constructor(activity: FragmentActivity) : super(activity)
+
+            override fun getItemCount() = 3
+
+            override fun createFragment(position: Int): Fragment {
+                when(position){
+                    0->{
+                        return Fragment1()
+                    }
+                    1->{
+                        return Fragment2()
+                    }
+                    2->{
+                        return Fragment3()
+                    }
+                }
+                return Fragment1()
+            }
+        }
+    fun showToast(Message:String){
+        Toast.makeText(this, Message,Toast.LENGTH_LONG).show()
+    }
+}
